4ba98070ff6cea0f6f993efa16156e1cd657ce1e
spiritface57 <abbasi.mahmoudreza@gmail.com>
2024-05-14 16:07:27 -0400

    correct form issues

--- resources/views/FrontPages/Errors/frontPageErrors.blade.php
+++ resources/views/FrontPages/Errors/frontPageErrors.blade.php
@@ -1,13 +1,13 @@
 @if ($errors->any())
-    <div class="bg-danger text-white m-2 p-4 rounded-3">
+    <div class="bg-danger text-white my-2 p-4 rounded-3">
         @foreach ($errors->all() as $error)
             <p>{{ $error }}</p>
         @endforeach
     </div>
 @endif
 @if (Session::has('error'))
-    <div class="bg-danger text-white m-2 p-4">{{ Session::get('error') }}</div>
+    <div class="bg-danger text-white my-2 p-4">{{ Session::get('error') }}</div>
 @endif
 @if (Session::has('success'))
-    <div class="bg-success text-white m-2 p-4 rounded-3">{{ Session::get('success') }} </div>
+    <div class="bg-success text-white my-2 p-4 rounded-3">{{ Session::get('success') }} </div>
 @endif
