4c39fab73ad3199b4ff879e71433ebe696c39aa5
spiritface57 <abbasi.mahmoudreza@gmail.com>
2024-07-16 14:38:14 -0400

    change route

--- resources/views/layouts/contact.blade.php
+++ resources/views/layouts/contact.blade.php
@@ -18,6 +18,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>{{ config('app.name', 'Laravel') }}</title>
+    <link rel="icon" href="{{ Vite::asset('resources/assets/img/favicon.jpg') }}">
     <link rel="dns-prefetch" href="//fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=Nunito" rel="stylesheet">
     <link
